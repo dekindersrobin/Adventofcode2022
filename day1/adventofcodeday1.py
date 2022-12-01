aefa60c6b9da878ebe833c2ef5f27c25152eb37c
Robin de Kinders <robin.de.kinders@traxion.com>
2022-12-01 15:30:05 +0100

    added second challenge solution

--- day1/adventofcodeday1.py
+++ day1/adventofcodeday1.py
@@ -2,18 +2,30 @@ elves = []
 greatest_calories = 0
 calorie_sum = 0
 
-with open("input.txt", "r") as input:
+with open("./day1/input.txt", "r") as input:
     input_lines = input.readlines()
 
-for line in input_lines:
-    if line.rstrip("\n") == "":
-        elves.append(calorie_sum)
-        calorie_sum = 0
-    else:
-        calorie_sum += int(line)
+def addElvesCalories(calorie_sum):
+    for line in input_lines:
+        if line.rstrip("\n") == "":
+            elves.append(calorie_sum)
+            calorie_sum = 0
+        else:
+            calorie_sum += int(line)
 
-for calorie in elves:
-    if calorie > greatest_calories:
-        greatest_calories = calorie
+def getGreatestCalories(greatest_calories):
+    for calorie in elves:
+        if calorie > greatest_calories:
+            greatest_calories = calorie
+    print(greatest_calories)
 
-print(greatest_calories)
+def getTopThreeElvesCalories():
+    elves.sort()
+    elves.reverse()
+    print(sum(elves[:3]))
+
+addElvesCalories(calorie_sum)
+print("First challenge outcome: ")
+getGreatestCalories(greatest_calories)
+print("Second challenge outcome: ")
+getTopThreeElvesCalories()
